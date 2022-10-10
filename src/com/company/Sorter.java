08a846471982037d7c5d5c6cb9648e227ed1e4f1
abulr <s202042560@kfupm.edu.sa>
2022-10-10 18:01:14 +0300

    Second Push (A: Adding selection sort method)

--- src/com/company/Sorter.java
+++ src/com/company/Sorter.java
@@ -6,4 +6,18 @@ public class Sorter {
 	// write your code here
         System.out.println("TEST2");
     }
+    void selectionSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
